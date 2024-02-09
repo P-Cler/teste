5b2a0059ef83c3b3bb1104b4aee4de5da9673638
P-Cler <156240431+P-Cler@users.noreply.github.com>
2024-02-09 01:55:54 -0300

    Add files via upload

---
+++ conexao.php
@@ -0,0 +1,8 @@
+<?php
+$servidor = "monorail.proxy.rlwy.net";
+$usuario = "root";
+$senha = "Gee1G1EDacdDHG2HGhG2c44fe1DBCGe-";
+$dbname = "railway";
+
+//Criar a conexao
+$conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
---
+++ processa.php
@@ -0,0 +1,21 @@
+<?php
+session_start();
+include_once("conexao.php");
+
+$nome = $_POST['nome'];
+
+$email = $_POST['email'];
+
+//echo "Nome: $nome <br>";
+//echo "E-mail: $email <br>";
+
+$result_usuario = "INSERT INTO testagem (nome, email) VALUES ('$nome', '$email')";
+$resultado_usuario = mysqli_query($conn, $result_usuario);
+
+if(mysqli_insert_id($conn)){
+	$_SESSION['msg'] = "<p style='color:green;'>Usuário cadastrado com sucesso</p>";
+	header("Location: index.php");
+}else{
+	$_SESSION['msg'] = "<p style='color:red;'>Usuário não foi cadastrado com sucesso</p>";
+	header("Location: index.php");
+}

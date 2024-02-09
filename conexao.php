9d5e5d348dfe8442488c71a6d98402aeb11beaf6
P-Cler <156240431+P-Cler@users.noreply.github.com>
2024-02-09 02:24:00 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -3,6 +3,7 @@ $servidor = "monorail.proxy.rlwy.net";
 $usuario = "root";
 $senha = "Gee1G1EDacdDHG2HGhG2c44fe1DBCGe-";
 $dbname = "railway";
+$port ="22992";
 
 //Criar a conexao
-$conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
+$conn = mysqli_connect($servidor, $usuario, $senha, $dbname, $port);
